724ca99e338fc94386a2b02c6840d23ee85fbf78
Jeremy Zongker <jeremy@trilitech.com>
2026-02-03 20:05:02 -0600

    Got working on iOS

--- metro.config.js
+++ metro.config.js
@@ -1,20 +1,8 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 const { getSentryExpoConfig } = require('@sentry/react-native/metro');
-const path = require('path');
 
 const config = getSentryExpoConfig(__dirname);
-
-// Support for file:// linked packages (ContentProviderHelper)
-const contentProviderHelperPath = path.resolve(__dirname, '../ContentProviderHelper');
-config.watchFolders = [contentProviderHelperPath];
-config.resolver.nodeModulesPaths = [
-  path.resolve(__dirname, 'node_modules'),
-  path.resolve(contentProviderHelperPath, 'node_modules'),
-];
-config.resolver.extraNodeModules = {
-  '@churchapps/content-provider-helper': contentProviderHelperPath,
-};
 
 // When enabled, the optional code below will allow Metro to resolve
 // and bundle source files with TV-specific extensions
