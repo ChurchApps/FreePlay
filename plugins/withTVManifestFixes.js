f4c33a9d36dab4fce8ef2514abc681c286a033f9
Jeremy Zongker <jeremy@zongker.net>
2026-05-10 21:12:59 -0500

    Fixing ios issues

--- plugins/withTVManifestFixes.js
+++ plugins/withTVManifestFixes.js
@@ -1,8 +1,10 @@
-const { withDangerousMod } = require("expo/config-plugins");
+const { withDangerousMod, withInfoPlist } = require("expo/config-plugins");
 const fs = require("fs");
 const path = require("path");
 
-function withTVManifestFixes(config) {
+const isTV = () => /^(1|true)$/i.test(process.env.EXPO_TV ?? "");
+
+function withAndroidTVManifestFixes(config) {
   return withDangerousMod(config, [
     "android",
     async (config) => {
@@ -40,6 +42,23 @@ function withTVManifestFixes(config) {
       return config;
     },
   ]);
+}
+
+// LSRequiresIPhoneOS makes App Store Connect validate the build as iOS,
+// which then rejects every tvOS-shaped key (CFBundlePrimaryIcon string,
+// DTPlatformName=appletvos, tvOS provisioning profile). Strip it on tvOS.
+function withTVInfoPlistFixes(config) {
+  if (!isTV()) return config;
+  return withInfoPlist(config, (config) => {
+    delete config.modResults.LSRequiresIPhoneOS;
+    return config;
+  });
+}
+
+function withTVManifestFixes(config) {
+  config = withAndroidTVManifestFixes(config);
+  config = withTVInfoPlistFixes(config);
+  return config;
 }
 
 module.exports = withTVManifestFixes;
